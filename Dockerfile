7f24759e2367d2309e0da84051a721b73bd133d4
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2023-10-19 17:39:17 +0100

    Add Docker and vs code extension

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:18-alpine
+
+RUN npm i -g pnpm
+
+WORKDIR /usr/src/app
+
+COPY package.json pnpm-lock.yaml ./
+RUN pnpm install --frozen-lockfile
+
+COPY . .
+
+# For Documentation Purposes
+EXPOSE 3000
+
+
+CMD [ "pnpm", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello world");
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev')); // logger
+
+app.get('/', async (req, res) => {
+  res.json({ hello: 'MuyiwaDoskyyy' });
+});
+
+const PORT = Number(process.env.PORT ?? 3000);
+app.listen(PORT, '0.0.0.0', () => {
+  // Running your server on 0.0.0.0 (as opposed to 127.0.0.1 which is the default) is required for Docker.
+  console.log(`Server running at http://localhost:${PORT}`);
+});
