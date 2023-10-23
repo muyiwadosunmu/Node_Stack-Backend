fb33bed495879023d897916b705fc587216e8b81
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2023-10-23 15:06:02 +0100

    Was able to setup graphql successfully

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,45 @@
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault,
+} from 'apollo-server-core';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
 import express from 'express';
+import http from 'http';
 import morgan from 'morgan';
+import resolvers from './graphql/resolvers';
+import schema from './graphql/schema';
 import db from './modules/db';
 
 const app = express();
-app.use(morgan('dev')); // logger
+app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', async (req, res) => {
-  const resp = await db.submissions.findMany();
-  res.status(200).json({ resp });
+  const submissions = await db.submission.findMany();
+  res.json(submissions);
 });
 
-const PORT = Number(process.env.PORT ?? 3000);
-app.listen(PORT, '0.0.0.0', () => {
-  // Running your server on 0.0.0.0 (as opposed to 127.0.0.1 which is the default) is required for Docker.
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  const port = Number(process.env.PORT ?? 3000);
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ host: '0.0.0.0', port }, resolve)
+  );
+  console.log(
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+  );
+};
+startServer();
--- src/modules/db.ts
+++ src/modules/db.ts
@@ -1,7 +1,34 @@
 import { PrismaClient } from '@prisma/client';
 import { nanoid } from 'nanoid';
-
 const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
 export default db;
 
 export const genId = () => nanoid(16);
+
+const seedDatabase = async () => {
+  if ((await db.submission.count()) === 0) {
+    await db.submission.createMany({
+      data: [
+        {
+          id: genId(),
+          submittedAt: new Date(),
+          data: {
+            name: 'Muyiwa Dosunmu',
+            twitter: 'muyiwadosunmu',
+          },
+        },
+        {
+          id: genId(),
+          submittedAt: new Date(),
+          data: {
+            name: 'Joshua Dada',
+            twitter: 'joshdad',
+          },
+        },
+      ],
+    });
+  }
+  // You can add additional logic outside the if condition here.
+};
+
+seedDatabase();
